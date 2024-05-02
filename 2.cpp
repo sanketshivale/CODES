ed2b8334239f759c9a61fcfa32d613ca51cde7c4
sanketshivale <shivalesanket21@gmail.com>
2024-05-03 02:05:30 +0530

    Refactor Sort class to remove unnecessary class declaration and use global functions instead

--- 2.cpp
+++ 2.cpp
@@ -3,25 +3,20 @@
 #include <omp.h>
 using namespace std;
 
-class Sort {
-    public:
-        void bubbleSort(int arr[], int n);
-        void display(int arr[], int n);
-        void mergeSort(int arr[], int low, int high);
-        void merge(int arr[], int low, int mid, int high);
-};
 
-void Sort::bubbleSort(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        #pragma omp parallel shared(arr)
-        for (int j = 0; j < n; j++) {
-            if (arr[i] < arr[j])
-                swap(arr[i], arr[j]);
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        #pragma omp parallel for shared(arr)
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
         }
     }
 }
 
-void Sort::merge(int arr[], int low, int mid, int high) {
+void merge(int arr[], int low, int mid, int high) {
     int n = high - low + 1;
     vector<int> temp(n);
     int i = low, j = mid+1, k = 0;
@@ -47,26 +42,26 @@ void Sort::merge(int arr[], int low, int mid, int high) {
     }
 }
 
-void Sort::mergeSort(int arr[], int low, int high) {
+void mergeSort(int arr[], int low, int high) {
     if (low < high) {
         int mid = low + (high - low) / 2;
         #pragma omp parallel sections
         {
             #pragma omp section
             {
-                this->mergeSort(arr, low, mid);
+                mergeSort(arr, low, mid);
             }
             
             #pragma omp section
             {
-                this->mergeSort(arr, mid+1, high);
+                mergeSort(arr, mid+1, high);
             }
         }
-        this->merge(arr, low, mid, high);
+        merge(arr, low, mid, high);
     }
 }
 
-void Sort::display(int arr[], int n) {
+void display(int arr[], int n) {
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
@@ -79,19 +74,17 @@ int main()
     int arr[] = {23,34,2,45,12,5};
     int n = sizeof(arr)/sizeof(arr[0]);
     
-    Sort s;
-    
     // double start = omp_get_wtime();
     // s.bubbleSort(arr, n);
     // double end = omp_get_wtime();
     // cout << "Parallel Bubble sort duration: " << (end - start) << endl;
     
     double start = omp_get_wtime();
-    s.mergeSort(arr, 0, n-1);
+    mergeSort(arr, 0, n-1);
     double end = omp_get_wtime();
     cout << "Parallel Merge sort duration: " << (end - start) << endl;
     
-    s.display(arr, n);
+    display(arr, n);
     
     return 0;
 }
